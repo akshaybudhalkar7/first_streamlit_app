ce32eb1577180eb941288566ff51bec62bb7ec08
akshaybudhalkar7 <100187289+akshaybudhalkar7@users.noreply.github.com>
2023-09-22 19:58:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,9 @@ streamlit.text('Hi Trupti What are you making today in dinner ?? ')
 
 list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(list)
+
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(list.index))
+
+# Display the table on the page.

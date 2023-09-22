0c8a98613996e2db85b493f019a702e640162db2
akshaybudhalkar7 <100187289+akshaybudhalkar7@users.noreply.github.com>
2023-09-22 19:56:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
+import pandas
+
 streamlit.title('My parents healthy dinner')
 streamlit.text('Hi Trupti What are you making today in dinner ?? ')
+
+list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(list)

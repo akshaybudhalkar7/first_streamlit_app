27b37184dc915af28d99ce4289f1d072a1f6fec4
akshaybudhalkar7 <100187289+akshaybudhalkar7@users.noreply.github.com>
2023-09-24 13:03:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ streamlit.dataframe(fruit_to_show)
 
 streamlit.header('Fruitvice fruit added')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
